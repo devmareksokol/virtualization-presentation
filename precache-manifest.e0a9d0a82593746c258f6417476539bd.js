5d5ba5ba4e2b41682822a34515cfb77b8e3bed49
Marek Sokol <devmareksokol@gmail.com>
2020-10-01 12:56:52 +0200

    Updates

---
+++ precache-manifest.e0a9d0a82593746c258f6417476539bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fcb502b181ce4b5046ac14c8b62ec24",
+    "url": "/virtualization-presentation/index.html"
+  },
+  {
+    "revision": "78045342623ea7863dc9",
+    "url": "/virtualization-presentation/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7df20b42ce13f36464b3",
+    "url": "/virtualization-presentation/static/js/2.c15902ab.chunk.js"
+  },
+  {
+    "revision": "f74a6debb7da89320918198b818026f8",
+    "url": "/virtualization-presentation/static/js/2.c15902ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78045342623ea7863dc9",
+    "url": "/virtualization-presentation/static/js/main.bc139b77.chunk.js"
+  },
+  {
+    "revision": "7525008725d72d8e0307",
+    "url": "/virtualization-presentation/static/js/runtime-main.f421d5e4.js"
+  }
+]);
